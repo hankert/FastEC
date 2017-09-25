be02e326ccba4f79ea63f2e114688f57ac2e3376
hanker <jinhui0106@126.com>
2017-09-25 18:14:01 +0800

    添加jar包

---
+++ latte-core/src/main/java/com/flowbank/latte/activities/ProxyActivity.java
@@ -0,0 +1,14 @@
+package com.flowbank.latte.activities;
+
+import me.yokeyword.fragmentation.SupportActivity;
+
+/**
+ * @auther jh
+ * @des ${TODO}
+ * Created by J.H on 2017/9/25.
+ */
+
+public class ProxyActivity extends SupportActivity{
+
+
+}
---
+++ latte-core/src/main/java/com/flowbank/latte/delegates/BaseDelegate.java
@@ -0,0 +1,35 @@
+package com.flowbank.latte.delegates;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import me.yokeyword.fragmentation_swipeback.SwipeBackFragment;
+
+/**
+ * @auther jh
+ * @des ${TODO}
+ * Created by J.H on 2017/9/25.
+ */
+
+public abstract class BaseDelegate extends SwipeBackFragment {
+
+    public abstract Object setLayout();
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = null;
+        if (setLayout() instanceof Integer){
+            rootView = inflater.inflate((Integer) setLayout(), container, false);
+        }else if (setLayout() instanceof View){
+            rootView = (View) setLayout();
+        }
+        if (rootView != null){
+
+        }
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
